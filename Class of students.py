02db5d0ee2fae6a4d8db14be8cbee350228d1a8f
Shao-Zhe <50443882+Shao-Zhe@users.noreply.github.com>
2019-06-06 17:05:15 +0800

    Update Class of students.py

--- Class of students.py
+++ Class of students.py
@@ -9,37 +9,60 @@ class CalUtils:
 
     def calAvgHeight(self):
 
-        input_new_user = input("Do you want to input new student name? Y/N: ")
+      file = open('listOfStudentHeight.txt', 'r')
 
-        if input_new_user == 'Y' :
-            file = open('listOfStudentHeight.txt', 'a')
+      for line in file.readlines():
+        data = line.split('\t')
+        self.name.append(data[0])
+        self.height.append(float(data[1]))
 
-            name = input('Enter a name: ')
-            height = input('Enter height: ')
+      self.total_student_count = len(self.name)
 
-            file.write(name + '\t' + height)
+      self.total_student_height = sum(self.height)
 
-            file.close()
-
-
-        file = open('listOfStudentHeight.txt', 'r')
-
-        for line in file.readlines():
-            data = line.split('\t')
-            self.name.append(data[0])
-            self.height.append(float(data[1]))
-
-        self.total_student_count = len(self.name)
-
-        self.total_student_height = sum(self.height)
-
-        avg_height = self.total_student_height / self.total_student_count
+      avg_height = self.total_student_height / self.total_student_count
 
 
-        print(round(avg_height, 2))
+      file.close()
 
-        file.close()
+      print(round(avg_height, 2))
 
+
+    def main(self):
+      input_new_user = input("Do you want to input new student name? Y/N: ")
+
+      if input_new_user == 'Y' :
+        file = open('listOfStudentHeight.txt', 'a')
+        name = input('Enter a name: ')
+        height = input('Enter height: ')
+        file.write(name + '\t' + height)
+          
+      else:
+
+        exit()
+
+      file.close()
+
+
+      file = open('listOfStudentHeight.txt', 'r')
+
+      for line in file.readlines():
+        data = line.split('\t')
+        self.name.append(data[0])
+        self.height.append(float(data[1]))
+
+      self.total_student_count = len(self.name)
+
+      self.total_student_height = sum(self.height)
+
+      new_avg_height = self.total_student_height / self.total_student_count
+
+
+      file.close()
+
+      print(round(new_avg_height, 2))
 
 student = CalUtils()
 student.calAvgHeight()
+student.main()
+
